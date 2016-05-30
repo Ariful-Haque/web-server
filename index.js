ff8878179a8cb2f3f43b33ae7010f3d530299feb
Ariful Haque <arifulhaque@Arifuls-MacBook-Air.local>
2016-05-30 13:04:46 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+
+
+var port = 8080;
+
+var middleware = {
+	checkAuthentication: function(req, res, next) {
+		console.log('Checking Auth....');
+		next();
+	},
+	logger: function(req, res, next) {
+		console.log('Request: ' + req.method + ' ' + req.originalUrl + ' made on day: ' + new Date().toString());
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.checkAuthentication, function(req, res) {
+	res.send('<h3>About Us</h3')
+})
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(port, function() {
+	console.log('Server is running on port: ' + port);
+})
