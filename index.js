ce5c1588588a485c162ec6a8b9440fb32bc98f52
Ariful Haque <ahmarufbd@gmail.com>
2016-05-30 19:40:40 +0300

    test commit

--- index.js
+++ index.js
@@ -4,16 +4,7 @@ var middleware = require('./middleware');
 
 var port = 8080;
 
-var middleware = {
-	checkAuthentication: function(req, res, next) {
-		console.log('Checking Authorizat....');
-		next();
-	},
-	logger: function(req, res, next) {
-		console.log('Request: ' + req.method + ' ' + req.originalUrl + ' made on day: ' + new Date().toString());
-		next();
-	}
-};
+
 
 app.use(middleware.logger);
 
